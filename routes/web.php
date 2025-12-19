21bedde8b28bc77020775546d1fde5cc75e75a94
Rafał Stróżyk <rafal.strozyk@onet.pl>
2025-12-20 00:35:30 +0100

    fix: allow for vue router takeover

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+ */
+Route::get('{any}', function(Request $request) {
+    return view('ftontend.index');
+})->where('any', '^(?!assets|api|admin|storage).*')
